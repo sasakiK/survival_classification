29ebadbc74b1cf6e4b607108535151e5da84927b
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-05-15 01:43:07 +0900

    initial

---
+++ src/data/make_dataset.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+import os
+
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+
+    # load dataset
+    df= pd.read_csv(input_filepath + "/train.csv").replace("male",0).replace("female",1)
+
+    # treat missing data
+    df["Age"].fillna(df.Age.median(), inplace=True)
+
+    # make variables
+    df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+    df2 = df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+
+    df2.to_csv(output_filepath)
+    print(os.getcwd())
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
+
+if __name__ == '__main__':
+
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    # load_dotenv(find_dotenv())
+
+    main(input_filepath = "/data/raw/", output_filepath = "/data/processed/")
